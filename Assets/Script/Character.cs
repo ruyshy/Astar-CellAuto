683aaa5481695d6b4f83bfcb22810051b227fb27
ruyshy <47741373+ruyshy@users.noreply.github.com>
2024-03-23 20:51:41 +0900

    Initial commit

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    #region VAR
+    public GameObject gM;
+    public GameObject cCamera;
+    private const int V = 0;
+    Vector2 move, mouse;
+    Rigidbody2D rb;
+    private int INext = 0;
+    float angle;
+    #endregion
+
+    #region Method
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Move(float ad, float ws)
+    {
+        move.Set(ad, ws);
+        move = move.normalized * 5.0f * Time.deltaTime;
+        rb.MovePosition(transform.position + new Vector3(move.x, move.y, 0.0f)); ;
+    }
+    #endregion
+
+    #region Collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Wall"))
+        {
+            Vector2 Direction = transform.position - collision.transform.position;
+
+            gameObject.transform.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * 2.0f);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Floor"))
+        {
+            gM.GetComponent<MapCreate>().MapDelete(ref INext);
+        }
+    }
+    #endregion
+
+    #region Update
+    private void Update()
+    {
+        cCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - gameObject.transform.position.y, mouse.x - gameObject.transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        float ad = Input.GetAxisRaw("Horizontal");
+        float ws = Input.GetAxisRaw("Vertical");
+
+        Move(ad, ws);
+    }
+    #endregion
+
+}
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //직렬화
+public class Node
+{
+    public Node(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
+
+    public bool isWall;
+    public Node ParentNode;
+
+    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
+    public int x, y, G, H;
+    public int F { get { return G + H; } }
+}
+
+
+public class Enemy : MonoBehaviour
+{
+    #region VAR
+    public Vector2Int bottomLeft, topRight, startPos, targetPos;
+    public List<Node> FinalNodeList;
+    public bool allowDiagonal, dontCrossCorner;
+
+    int sizeX, sizeY;
+    Node[,] NodeArray;
+    Node StartNode, TargetNode, CurNode;
+    List<Node> OpenList, ClosedList;
+
+    private Transform Taget;
+    private bool Nevi = false;
+    float angle;
+    #endregion
+
+    #region Trigger Collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            Nevi = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            Nevi = false;
+    }
+    #endregion
+
+    #region A*
+    public void PathFinding()
+    {
+        startPos = Vector2Int.RoundToInt(transform.position);
+        targetPos = Vector2Int.RoundToInt(Taget.transform.position);
+
+        sizeX = topRight.x - bottomLeft.x + 1;
+        sizeY = topRight.y - bottomLeft.y + 1;
+        NodeArray = new Node[sizeX, sizeY];
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                bool isWall = false;
+                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f))
+                    if (col.gameObject.layer == LayerMask.NameToLayer("Wall")) isWall = true;
+
+                NodeArray[i, j] = new Node(isWall, i + bottomLeft.x, j + bottomLeft.y);
+            }
+        }
+
+        StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.y - bottomLeft.y];
+        TargetNode = NodeArray[targetPos.x - bottomLeft.x, targetPos.y - bottomLeft.y];
+
+        OpenList = new List<Node>() { StartNode };
+        ClosedList = new List<Node>();
+        FinalNodeList = new List<Node>();
+
+
+        while (OpenList.Count > 0)
+        {
+            CurNode = OpenList[0];
+            for (int i = 1; i < OpenList.Count; i++)
+                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H) CurNode = OpenList[i];
+
+            OpenList.Remove(CurNode);
+            ClosedList.Add(CurNode);
+
+
+            if (CurNode == TargetNode)
+            {
+                Node TargetCurNode = TargetNode;
+                while (TargetCurNode != StartNode)
+                {
+                    FinalNodeList.Add(TargetCurNode);
+                    TargetCurNode = TargetCurNode.ParentNode;
+                }
+                FinalNodeList.Add(StartNode);
+                FinalNodeList.Reverse();
+
+                return;
+            }
+
+
+            // 대각
+            if (allowDiagonal)
+            {
+                OpenListAdd(CurNode.x + 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y - 1);
+                OpenListAdd(CurNode.x + 1, CurNode.y - 1);
+            }
+
+            // 직선
+            OpenListAdd(CurNode.x, CurNode.y + 1);
+            OpenListAdd(CurNode.x + 1, CurNode.y);
+            OpenListAdd(CurNode.x, CurNode.y - 1);
+            OpenListAdd(CurNode.x - 1, CurNode.y);
+        }
+    }
+    void OpenListAdd(int checkX, int checkY)
+    {
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
+        {
+            if (allowDiagonal) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+            if (dontCrossCorner) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+
+            Node NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
+            int MoveCost = CurNode.G + (CurNode.x - checkX == 0 || CurNode.y - checkY == 0 ? 10 : 14);
+
+
+            if (MoveCost < NeighborNode.G || !OpenList.Contains(NeighborNode))
+            {
+                NeighborNode.G = MoveCost;
+                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(NeighborNode.y - TargetNode.y)) * 10;
+                NeighborNode.ParentNode = CurNode;
+
+                OpenList.Add(NeighborNode);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Gizmos UI
+    void OnDrawGizmos()
+    {
+        if (FinalNodeList.Count != 0) for (int i = 0; i < FinalNodeList.Count - 1; i++)
+                Gizmos.DrawLine(new Vector2(FinalNodeList[i].x, FinalNodeList[i].y), new Vector2(FinalNodeList[i + 1].x, FinalNodeList[i + 1].y));
+    }
+    #endregion
+    private void Start()
+    {
+        Taget = GameObject.Find("Character").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Nevi == true)
+        {
+            Taget = GameObject.Find("Character").transform;
+            angle = Mathf.Atan2(Taget.transform.position.y - gameObject.transform.position.y, Taget.transform.position.x - gameObject.transform.position.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+            PathFinding();
+
+            if (FinalNodeList.Count != 0)
+            {
+                if (FinalNodeList.Count == 1) return;
+
+                Vector2 FinalNodePos = new Vector2(FinalNodeList[1].x, FinalNodeList[1].y);
+                transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y,-1.0f), FinalNodePos, 0.07f);
+
+                if ((Vector2)transform.position == FinalNodePos) FinalNodeList.RemoveAt(0);
+            }
+        }
+    }
+}
